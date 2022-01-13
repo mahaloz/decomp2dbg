ebbb4276709147aefff96e8201d4a712ee283c7c
Zion <zionbasque@gmail.com>
2022-01-12 22:54:42 -0700

    Fix rebasing bugs in angr-decompiler plugin (#18)
    * fixed a rebasing bug

--- decompilers/d2g_angr/server.py
+++ decompilers/d2g_angr/server.py
@@ -21,6 +21,17 @@ class AngrDecompilerServer:
     # Private
     #
 
+    def rebase_addr(self, addr, down=False):
+        rebased = addr
+        base_addr = self._instance.project.loader.min_addr
+
+        if down:
+            rebased -= base_addr
+        elif addr < base_addr:
+            rebased += base_addr
+
+        return rebased
+
     def _decompile_function(self, func):
         """
         Taken directly from BinSync
@@ -61,14 +72,11 @@ class AngrDecompilerServer:
 
         Always returns a dict with the defined keys below, which may have None as their values.
         """
-
         resp = {
             "decompilation": None
         }
-        self._workspace.log(f"Attempting decompile for {hex(addr)}")
-        if addr < self._instance.project.loader.min_addr:
-            addr += self._instance.project.loader.min_addr
 
+        addr = self.rebase_addr(addr)
         func_addr = self._instance.cfg.get_any_node(addr, anyaddr=True).function_address
         func = self._instance.kb.functions[func_addr]
         decomp = self._decompile_function(func)
@@ -97,6 +105,8 @@ class AngrDecompilerServer:
             "args": {},
             "stack_vars": {}
         }
+
+        addr = self.rebase_addr(addr)
         func_addr = self._instance.cfg.get_any_node(addr, anyaddr=True).function_address
         func = self._instance.kb.functions[func_addr]
         decomp = self._decompile_function(func)
@@ -113,7 +123,7 @@ class AngrDecompilerServer:
     def function_headers(self):
         resp = {}
         for addr, func in self._instance.kb.functions.items():
-            resp[str(addr)] = {
+            resp[str(self.rebase_addr(addr, down=True))] = {
                 "name": func.name,
                 "size": func.size
             }
