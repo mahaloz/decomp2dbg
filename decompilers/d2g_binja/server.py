8377cc18307d000ab2042313619b8c8704f158bf
mmaekr <mattym7@gmail.com>
2022-01-18 12:32:24 -0500

    added binja function data, function headers, global vars, and structs

--- decompilers/d2g_binja/server.py
+++ decompilers/d2g_binja/server.py
@@ -62,26 +62,90 @@ class BinjaDecompilerServer:
             "stack_vars": {}
         }
 
+        funcs = self.bv.get_functions_containing(addr)
+        if not funcs:
+            return resp
+
+        func = funcs[0]
+
+        # get stack vars
+        stack_vars = {}
+        for stack_var in func.stack_layout:
+            offset = abs(stack_var.storage)
+            stack_vars[str(offset)] = {
+                "name": stack_var.name,
+                "type": str(stack_var.type)
+            }
+
+        # get args
+        func_args = {}
+        for idx, param in enumerate(func.function_type.parameters):
+            func_args[str(idx)] = {
+                "name": param.name,
+                "type": str(param.type)
+            }
+
+        resp["args"] = func_args
+        resp["stack_vars"] = stack_vars
+
         return resp
+
 
     def function_headers(self):
         resp = {}
 
+        for func in self.bv.functions:
+
+            # Skip everything besides FunctionSymbol
+            if func.symbol.type != SymbolType.FunctionSymbol:
+                continue
+
+            resp[str(func.start)] = {
+                "name": func.name,
+                "size": func.total_bytes
+            }
+
         return resp
+
 
     def global_vars(self):
         resp = {}
+        known_segs = [".data", ".bss"]
+        for seg_name in known_segs:
+            seg = self.bv.get_section_by_name(seg_name)
+            for seg_ea in range(seg.start, seg.end):
+                if len(list(self.bv.get_code_refs(seg_ea))) < 1:
+                    # no refs
+                    continue
+
+                var = self.bv.get_data_var_at(seg_ea)
+                resp[str(seg_ea)] = {
+                    "name": var.name
+                }
 
         return resp
 
+
     def structs(self):
         resp = {}
+
+        # tuple of structure name and StructureType
+        for t in self.bv.types.items():
+            struct_name = t[0]
+            struct = t[1]
+            resp[struct_name] = {
+                "size": struct.width
+            }
+            for member in struct.members:
+                resp[struct_name][member.name] = {
+                    "offset": member.offset,
+                    "size": len(member)
+                }
 
         return resp
 
     def breakpoints(self):
         resp = {}
-
         return resp
 
     #
